65c5a2615442b5351166a6189d617d01277de7d5
Andronio2 <42270003+Andronio2@users.noreply.github.com>
2022-02-25 19:00:29 +0600

    fix: Исправил ошибку получения селектора цены

--- Aliexpress item getter.user.js
+++ Aliexpress item getter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Aliexpress item getter
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  Получает ID, название, цену и конфигурацию
 // @author       Andronio
 // @homepage     https://github.com/Andronio2/Aliexpress-item-getter
@@ -110,7 +110,7 @@
         }
         item_conf = item_conf.slice(0, -1);
         let price = document.querySelector('.product-price-value,  .product-price-current');
-        if (!price) document.querySelector('.uniform-banner-box-price, .Product_UniformBanner__uniformBannerBoxPrice__o5qwb');
+        if (!price) price = document.querySelector('.uniform-banner-box-price, .Product_UniformBanner__uniformBannerBoxPrice__o5qwb');
         price = price.textContent.match(/\d{1,3}\s\d{3},\d{2}|\d{1,3},\d{3}\.\d{2}|\d{1,3}[\.,]\d{2}/);
         if (price && price.length === 0) {
             console.log('Не нашел цену');
